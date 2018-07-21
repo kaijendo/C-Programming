65e03f5df6610bb7cbdd2f6fa724b566bdc987ed
Thuy Truong Quang <thuytq@fsoft.com.vn>
2018-07-21 08:44:49 +0700

    Initial Commit

---
+++ Exam_21-07-2018/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Exam_21-07-2018
+//
+//  Created by Thuy Truong Quang on 7/21/18.
+//  Copyright © 2018 Thuy Truong Quang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
